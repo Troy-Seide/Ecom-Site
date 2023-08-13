55abb66f591a5a1a5ede7336ee25cacded1dd220
Troy Seide <troyseide@Troys-MacBook-Pro.local>
2023-08-12 20:30:30 -0400

    Added new components like shop and navbar, as well as  gave the site some structure.

--- Ecom-Site/src/app/app-routing.module.ts
+++ Ecom-Site/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { ShopComponent } from './shop/shop.component';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component:HomeComponent},
+  {path: 'login', component:LoginComponent},
+  {path: 'signup', component:SignupComponent},
+  {path: 'shop', component:ShopComponent},
+  {path: 'about', component:AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Ecom-Site/src/app/app.module.ts
+++ Ecom-Site/src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ShopComponent } from './shop/shop.component';
+import { FormsModule } from '@angular/forms';
+import { SignupComponent } from './signup/signup.component';
+import { AboutComponent } from './about/about.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    NavbarComponent,
+    ShopComponent,
+    SignupComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
